9c46d6f832c21d46f9f3996aa197ae158440adab
Brynjar Gretarsson <brynjar.gretarsson@appfolio.com>
2016-08-04 10:45:11 -0700

    Added a user model and use the first user from the database for basic auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221185908) do
+ActiveRecord::Schema.define(version: 20160804174213) do
 
   create_table "peer_reviews", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,13 @@ ActiveRecord::Schema.define(version: 20151221185908) do
     t.integer  "month"
     t.integer  "year"
     t.string   "people_sent"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
